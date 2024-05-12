b7ea8d440dd63dba704ff1310430a9741e9fc701
an9383 <an9383@gmail.com>
2024-05-12 14:41:27 +0900

    all

--- src/components/post/PostDetail.tsx
+++ src/components/post/PostDetail.tsx
@@ -1,7 +1,7 @@
-import type { Post } from '@/types/types'
-import Link from 'next/link' // Link 컴포넌트를 임포트합니다.
+import { Post } from '@/types/types'
+import Link from 'next/link'
+import React from 'react'
 
-// PostProps 타입을 정의합니다.
 interface PostProps {
     post: Post
 }
@@ -10,11 +10,16 @@ const PostDetail = ({ post }: PostProps) => {
     return (
         <div>
             <h2>{post.title}</h2>
-            <p>{post.content}</p>
             <p>{post.author}</p>
-            <p>{post.date}</p>
-            {/* && 연산자를 사용하여 tags가 존재하는 경우에만 출력합니다. */}
-            <ul>{post.tags && post.tags.map((tag, index) => <li key={index}>{tag}</li>)}</ul>
+            <p>{post.content}</p>
+            <ul>
+                {/* post.tags map() 돌려서 출력 */}
+                {/* 태그가 있을 경우에만 map() 돌림 */}
+                {post.tags?.map((tag, index) => (
+                    <li key={index}>{tag}</li>
+                ))}
+            </ul>
+            <div>{post.date}</div>
         </div>
     )
 }
