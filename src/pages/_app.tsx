773f73f93865afeee3cbdb4fbf68821564d12d29
an9383 <an9383@gmail.com>
2024-05-12 15:23:49 +0900

    all

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import Layout from '@/components/layout/Layout'
 import { AppProps } from 'next/app'
+import Layout from '@/components/layout/Layout'
+import app from '@/firebase/firebase' // Firebase SDK 구성을 위한 설정 파일 import
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
